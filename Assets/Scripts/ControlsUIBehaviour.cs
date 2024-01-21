a2cb79ad142effc4581130df1ef2a42c19c5ff3a
GodlyFoxy <31106898+GodlyFoxy@users.noreply.github.com>
2024-01-21 18:20:28 +0100

    Cleaned up some mess

--- Assets/Scripts/ControlsUIBehaviour.cs
+++ Assets/Scripts/ControlsUIBehaviour.cs
@@ -9,13 +9,6 @@ public class ControlsUIBehaviour : MonoBehaviour
     private TMP_Text speedometerKM;
     [SerializeField]
     private TMP_Text speedometerKnots;
-    float speed;
-
-    [SerializeField]
-    private Rigidbody _rb;
-
-
-
 
     [SerializeField]
     private Material speedLevel01Material;
@@ -26,34 +19,12 @@ public class ControlsUIBehaviour : MonoBehaviour
     [SerializeField]
     private Material speedLevel04Material;
 
-    [SerializeField]
-    private Color offColour;
-
-    private Color speedLevel01Colour;
-    private Color speedLevel02Colour;
-    private Color speedLevel03Colour;
-    private Color speedLevel04Colour;
-
     // I can probably make this a lot more efficient and neater
     void Start()
     {
-        // Store the colours
-        speedLevel01Colour = speedLevel01Material.color;
-        speedLevel02Colour = speedLevel02Material.color;
-        speedLevel03Colour = speedLevel03Material.color;
-        speedLevel04Colour = speedLevel04Material.color;
-
         // Set all colours to off
         UpdateSpeedLevel(-1f);
     }
-
-    private void FixedUpdate()
-    {
-        // It's physics, so it's FixedUpdate
-        //CalculateSpeed();
-    
-    }
-
 
     public void CalculateSpeed(float speed)
     {
@@ -81,70 +52,40 @@ public class ControlsUIBehaviour : MonoBehaviour
         {
             case > 0.75f:
                 // 4th level
-                //speedLevel04Material.color = speedLevel04Colour;
                 speedLevel04Material.EnableKeyword("_EMISSION");
                 break;
             case > 0.5f:
                 // 3rd level
-                //speedLevel03Material.color = speedLevel03Colour;
                 speedLevel04Material.DisableKeyword("_EMISSION");
                 speedLevel03Material.EnableKeyword("_EMISSION");
-
-
-                // Turn off 4th level
-                //speedLevel04Material.color = offColour;
                 break;
             case > 0.25f:
                 // 2nd level
-                //speedLevel02Material.color = speedLevel02Colour;
                 speedLevel03Material.DisableKeyword("_EMISSION");
                 speedLevel02Material.EnableKeyword("_EMISSION");
-
-                // Turn off 3rd level
-                //speedLevel03Material.color = offColour;
                 break;
             case > 0f:
                 // 1st level
-                //speedLevel01Material.color = speedLevel01Colour;
                 speedLevel02Material.DisableKeyword("_EMISSION");
                 speedLevel01Material.EnableKeyword("_EMISSION");
-
-                // Turn off 2nd level
-                //speedLevel02Material.color = offColour;
                 break;
             default:
-                // turn off all levels
-                //speedLevel01Material.color = offColour;
-                //speedLevel02Material.color = offColour;
-                //speedLevel03Material.color = offColour;
-                //speedLevel04Material.color = offColour;
-
-                // turn off emission
+                // turn off all emission
                 speedLevel01Material.DisableKeyword("_EMISSION");
                 speedLevel02Material.DisableKeyword("_EMISSION");
                 speedLevel03Material.DisableKeyword("_EMISSION");
                 speedLevel04Material.DisableKeyword("_EMISSION");
-
-
                 break;
         }
     }
 
     private void OnDisable()
     {
-        // Reset all colours to original
-        speedLevel01Material.color = speedLevel01Colour;
-        speedLevel02Material.color = speedLevel02Colour;
-        speedLevel03Material.color = speedLevel03Colour;
-        speedLevel04Material.color = speedLevel04Colour;
-
         // turn on emission
         speedLevel01Material.EnableKeyword("_EMISSION");
         speedLevel02Material.EnableKeyword("_EMISSION");
         speedLevel03Material.EnableKeyword("_EMISSION");
         speedLevel04Material.EnableKeyword("_EMISSION");
-
-        
     }
 
 
